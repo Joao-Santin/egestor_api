e46ee5510b0ac857559d6f67d37ba756be23f63f
Joao Rodrigues <vitor-santin@hotmail.com>
2025-06-25 17:25:22 -0300

    agora funcionou a coleta de dados, muito simples. tambem adicionei um readme para entender o que esta acontecendo.

---
+++ README.md
@@ -0,0 +1,7 @@
+OBJETIVOS:
+    [x] Aprender usar a API.(Fiz primeiras requisições.)
+    [] Fazer o levantamento de MRP com base nos arquivos levantados
+    [] Salvar conteúdos coletados em arquivo .csv ou json
+
+lixo:
+"codigo":16,"descricao":"BOMBA DE SUCCAO DE 1\" PARA BANHEIRO QU\u00cdMICO","codigoProprio":"2000016","estoque":25,"estoqueMinimo":0,"controlarEstoque":true,"margemLucro":0.01,"precoCusto":28.13,"precoVenda":28,"origemFiscal":0,"unidadeTributada":"UN","refEanGtin":"","ncm":"","excecaoIPI":0,"codigoCEST":"","pesoBruto":0,"pesoLiquido":0,"codigoGrupoTributos":1,"anotacoesNFE":"","anotacoesInternas":"","tipoProduto":"produto","tags":["Biplas","Bomba"],"dtCad":"2025-04-03 14:45:55","codCategoria":12,"updatedAt":"2025-06-25 11:15:59","listaImagens":[{"foto":{"link":"https:\/\/v4.egestor.com.br\/files\/?key=37975.500.9ad7.91fc.ee0"},"thumb":{"link":"https:\/\/v4.egestor.com.br\/files\/?key=37975.501.075e.a71e.9e1"}},{"foto":{"link":"https:\/\/v4.egestor.com.br\/files\/?key=37975.501.17af.902b.b03"},"thumb":{"link":"https:\/\/v4.egestor.com.br\/files\/?key=37975.501.2b5f.0521.e37"}}]}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use reqwest::Client;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct TokenRequest<'a> {
     grant_type: &'a str,
     personal_token: &'a str,
@@ -12,15 +12,23 @@ struct TokenRequest<'a> {
 #[derive(Deserialize, Debug)]
 struct TokenResponse{
     access_token: String,
-    token_type: String,
-    expires_in: i16,
-    refresh_token: String
+    // não sendo usados, por isso comentei, quando for usar, descomentar
+    //token_type: String,
+    //expires_in: i16,
+    //refresh_token: String
 }
-
+#[derive(Deserialize, Debug)]
+struct ProductResponse{
+    codigo: i16,
+    descricao: String,
+    codigoProprio: String,
+    estoque: i64,
+    tipoProduto: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Troque aqui pelo seu token real!
+    // aqui esta o token pessoal para poder retirar o token de acesso
     let token_str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJmZGZhNDZjZTkwNGQ4MTk1MDMwMjYyZWQ4MjhkYjllYiIsInN1YmRvbWluaW8iOiJiaXBsYXMiLCJjbGllbnQiOiI2Nzc4ODZjMTQ3ZGVkYjViNzkyNjNmY2E1M2QzMzVmNTNkNWE0ZjczIiwiY3JlYXRlZCI6MTc0NDgyODA4NH0=.PLJwUmU76hknWQplS1sYZkwZcMUIXx2iuQ11+KtBOyc=";
 
     let req_body = TokenRequest {
@@ -36,28 +44,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
-    let status = res_token.status();
+    //let status = res_token.status();
     //let body = res.text().await?;
+
     let response: TokenResponse = res_token.json().await?;
 
-    println!("Status: {}", status);
-    println!("Access Token: {}", response.access_token);
-    println!("Token_type: {}", response.token_type);
-    println!("expires_in: {}", response.expires_in);
-    println!("refresh_token: {}", response.refresh_token);
-    println!("-----------");
+    // Aqui alguns relacionados ao request de token
+    //println!("Status: {}", status);
+    //println!("Access Token: {}", response.access_token);
+    //println!("Token_type: {}", response.token_type);
+    //println!("expires_in: {}", response.expires_in);
+    //println!("refresh_token: {}", response.refresh_token);
+    //println!("-----------");
 
     let res = client
-        .post("https://api.egestor.com.br/api/v1/empresa")
+        .get("https://api.egestor.com.br/api/v1/produtos/16")
         .bearer_auth(response.access_token)
         .header("Content-Type", "application/json")
         .send()
         .await?;
+    let produtos_response: ProductResponse = res.json().await?;
+    println!("{}", produtos_response.codigo.to_string());
 
-    let status = res.status();
-    let body = res.text().await?;
-    println!("Status2: {}", status);
-    println!("Resposta: {}", body);
+    //let status = res.status();
+    //let body = res.text().await?;
+    //println!("Status2: {}", status);
+    //println!("Resposta: {}", body);
 
     Ok(())
 }
